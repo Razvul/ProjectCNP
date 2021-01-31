765998f0694d4037f3b1e2489ac01b346d7326c7
Andrei Vasiliu <andrei@anvaplus.com>
2021-01-31 18:26:18 +0100

    Add utilities class

---
+++ CommonCNP/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonCNP
+{
+    public static class Utilities
+    {
+        public static bool IsInteger(string a)
+        {
+            var result = int.TryParse(a, out int cifra);
+            return result;
+        }
+    }
+}
