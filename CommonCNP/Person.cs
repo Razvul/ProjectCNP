e138d4f9a31fdbc94fe8331e4ac94334eaaf9578
Agafitei Razvan <razvule@gmail.com>
2021-02-11 20:37:14 +0200

    [Task 22] Add class Person

---
+++ CommonCNP/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonCNP
+{
+    public class Person
+    {
+        public string Nume{ get; set; }
+        public string Prenume{ get; set; }
+        public string Sex{ get; set; }
+    }
+}
